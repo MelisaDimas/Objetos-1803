49ff995d87bf0d95e23f1a24f33a5bc12e93e930
MelisaDimas <32596219+MelisaDimas@users.noreply.github.com>
2018-05-28 19:48:12 -0500

    Add files via upload

---
+++ ProbarConstructores.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Constructores;
+
+/**
+ *
+ * @author T-101
+ */
+public class ProbarConstructores {
+    public static void main(String[] args){
+        Fruta f1=new Fruta();
+        System.out.println("El nombre de la fruta es "+f1.nombre);
+        System.out.println("el precio de la fruta es "+f1.precio);
+        
+      
+    }
+    
+}
